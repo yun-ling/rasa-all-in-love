2e6884e9986e6091949f6af6d8a08678bac6a01b
lingyun <lingyun.linker@bytedance.com>
2021-08-12 11:12:48 +0800

    [feature]1.添加rasa x的支持          2.添加中文分词器的支持          3.添加中文分词器的服务器

--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ make && make install
 ln -s /usr/local/python3/bin/python3.8 /usr/bin/python3.8
 ln -s /usr/local/python3/bin/pip3.8 /usr/bin/pip3.8
 pip3.8 install --user rasa
+pip3.8 install --user rasa[transformers]
+pip3.8 install --user rasa_chinese_service
+pip3.8 install --user rasa_chinese
+pip3.8 install rasa-x --extra-index-url https://pypi.rasa.com/simple
